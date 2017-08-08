cc3e45d30466cb0291921c29ccf15d68c26f7657
summerscar <shen.yus@163.com>
2017-08-08 10:44:47 +0800

    页面骨架完成

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rank" ref="rank">
+    rank
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@/common/stylus/variable"
+  @import "~@/common/stylus/mixin"
+
+
+</style>
