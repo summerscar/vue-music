819404527e22c6fc39da12fe3c364d9c85a93e21
summerscar <shen.yus@163.com>
2017-08-08 16:22:37 +0800

    推荐页面完成

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,10 +1,85 @@
 <template>
   <div class="recommend" ref="recommend">
+    <scroll class="recommend-content" :data="discList" ref="scroll">
+      <div>
+        <div class="slider-wrapper" v-if="recommends.length">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" class="needsclick">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img @load="loadImg" v-lazy="item.imgurl" width="60" height="60">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import {getRecommend, getDiscList} from '@/api/recommend'
+  import {ERR_OK} from '@/api/config'
+  import Slider from '@/base/slider/slider'
+  import Scroll from '@/base/scroll/scroll'
+  import Loading from '@/base/loading/loading'
 
+  export default {
+    components: {
+      Slider,
+      Scroll,
+      Loading
+    },
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      }
+    },
+    created() {
+      this._getRecommend()
+      this._getDiscList()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList() {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+            console.log(this.discList)
+          }
+        })
+      },
+      loadImg() {
+        if (!this.checkLoaded) {
+          this.$refs.scroll.refresh()
+          this.checkLoaded = true
+        }
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from './App'
 import router from './router'
 import '@/common/stylus/index.styl'
 import fastclick from 'fastclick'
+import vueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
+Vue.use(vueLazyLoad, {
+  loading: require('@/common/image/default.png')
+})
 
 fastclick.attach(document.body)
 /* eslint-disable no-new */
